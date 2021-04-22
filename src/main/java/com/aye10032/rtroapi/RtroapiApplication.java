c330cd74478e8e2a7d81bb5372e051f95a5d1426
Aye10032 <yeyu6_505@126.com>
2021-04-22 11:34:21 +0800

    Initial commit

---
+++ src/main/java/com/aye10032/rtroapi/RtroapiApplication.java
@@ -0,0 +1,13 @@
+package com.aye10032.rtroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtroapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtroapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aye10032/rtroapi/RtroapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aye10032.rtroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtroapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
